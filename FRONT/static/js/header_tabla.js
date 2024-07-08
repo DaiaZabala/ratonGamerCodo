3dd6bbfcf6b6b0ed79e6105140a79794b14e7257
DaiaZabala <daianazabala1303@gmail.com>
2024-07-07 22:46:59 -0300

    trabajo final

---
+++ FRONT/static/js/header_tabla.js
@@ -0,0 +1,22 @@
+//Contenido del encabezado
+let headerContent = `
+<header id="header">
+    <div>
+            <a href="http://127.0.0.1:5001/">
+    <img id="logo-header" src="static/img/logo.jpg" alt="logo">
+</a>
+
+    </div>
+
+    <p id="nombre-web">Ratón Gamer</p>
+    <p id="app-admin">App Admin</p>
+
+    <nav class="menu">
+            <a href="templates/ingresar_usuario.html">Registrar usuario</a>
+    </nav>
+</header>
+`
+
+
+//Agregar el contenido del encabezado al principio del body
+document.body.insertAdjacentHTML(`afterbegin`, headerContent);
